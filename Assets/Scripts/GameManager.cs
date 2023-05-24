8a39b65a968fc4eb171681445a19967ac69dd5e0
sj-jason-liu <sj.jason.liu@gmail.com>
2023-05-24 23:17:02 +0800

    Imported self-created 3D model key and placed it into 2D game scene & created first cutscene of go-to-bed & created pseudo code of GameManager.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,5 +22,28 @@ public class GameManager : MonoBehaviour
         _instance = this;
     }
 
+    //Go to bed cutscene counter
+        //if
+        //Go-to-bed Cutscene never been played
+        //play Go-to-bed Cutscene
 
+    //Object 1-closet checking bool
+    //Object 2-game console checking bool
+    //Object 3-key checking bool
+
+    //Object 1 checking function
+        //if object 1 has been collect     
+        //disable object 1 in 2D
+        //enable object 1 in 3D
+        //activate the moving platform 1
+
+    //Object 2 checking function
+        //if object 2 has been collect       
+        //disable object 2 in 2D
+        //enable object 2 in 3D
+        //activate moving platform 2
+
+    //Key checking function
+        //if object 3 has been collect
+        //room door cutscene can be activate by enter trigger
 }
