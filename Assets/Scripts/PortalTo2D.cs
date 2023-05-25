30a5f00f526e8bed301406225aa4af93d405837b
sj-jason-liu <sj.jason.liu@gmail.com>
2023-05-25 22:44:26 +0800

    Updated main menu panel buttons color.

--- Assets/Scripts/PortalTo2D.cs
+++ Assets/Scripts/PortalTo2D.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class PortalTo2D : MonoBehaviour
 {
-    private bool cutsceneHasPlayed;
+    private bool cutsceneHasPlayed, hasPlayerStepIn;
     [SerializeField]
     private GameObject goToBedCutscene, player2D, respawnPosition2D;
+
+    private void FixedUpdate()
+    {
+        if (hasPlayerStepIn && Input.GetKeyDown(KeyCode.E))
+        {
+            hasPlayerStepIn = false;
+            GameManager.Instance.PlayerCanControl = false; //disable player control
+            if (!cutsceneHasPlayed)
+            {
+                goToBedCutscene.SetActive(true);
+                cutsceneHasPlayed = true;
+            }
+            UIManager.Instance.WhiteOut();
+            player2D.transform.position = respawnPosition2D.transform.position; //reposition 2D player to start point
+            GameManager.Instance.Player2DCanControl = true;
+            Invoke("Switching", 5f); //switch to 2D scene after 3 seconds- UIManager
+        }
+    }
 
     private void OnTriggerStay(Collider other)
     {
         if(other.tag == "Player")
         {
-            Debug.Log("Player stepped in!");
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                GameManager.Instance.PlayerCanControl = false; //disable player control
-                if(!cutsceneHasPlayed)
-                {
-                    goToBedCutscene.SetActive(true);
-                    cutsceneHasPlayed = true;
-                }
-                UIManager.Instance.WhiteOut();               
-                player2D.transform.position = respawnPosition2D.transform.position; //reposition 2D player to start point
-                GameManager.Instance.Player2DCanControl = true;
-                Invoke("Switching", 5f); //switch to 2D scene after 3 seconds- UIManager
-            }
+            hasPlayerStepIn = true;
+            Debug.Log("Has Player step in? " + hasPlayerStepIn);           
         }    
     }
 
